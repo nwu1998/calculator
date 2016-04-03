1ed359e896a57b9c2561541d7e02ab459928b67c
nwu1998 <nancy_wu@cate.org>
2016-04-03 11:58:01 -0700

    decimal point working version

---
+++ calculatorDecimalPoint/calculator/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Nanyan Wu on 3/25/16.
+//  Copyright © 2016 Nanyan Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    @IBOutlet weak var display: UILabel!
+    
+    var userInTheMiddleOfTypingAnVariable = false
+    var brain = CalculatorBrain()
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userInTheMiddleOfTypingAnVariable {
+            if (display.text?.rangeOfString(".") == nil) || (display.text?.rangeOfString(".") != nil && digit != ".") {display.text = display.text! + digit}
+        } else {
+            display.text=digit
+            userInTheMiddleOfTypingAnVariable = true
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        if userInTheMiddleOfTypingAnVariable {
+            enter()
+        }
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                //well.. what if displayValue were optional
+                displayValue = 0
+            }
+        }
+    }
+    
+    @IBAction func enter() {
+        userInTheMiddleOfTypingAnVariable = false
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            //well.....
+            displayValue = 0
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userInTheMiddleOfTypingAnVariable = false
+        }
+    }
+    
+}
+
